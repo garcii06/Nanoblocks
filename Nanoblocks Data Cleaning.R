eb69ed9230d1a0eb820f36bbd32ee479ff713617
Israel Garcia Local <isragca@gmail.com>
2022-12-21 19:19:21 -0600

    Updated the pipes for cleaning of columns.

--- Nanoblocks Data Cleaning.R
+++ Nanoblocks Data Cleaning.R
@@ -19,24 +19,25 @@ nanoblocks <- read_csv("Nanoblocks_information.csv")
 # locale the names, all of them contains the Japanese name with romaji or Japanese.
 # clean the Korean part and the series name, between many other things.
 
-## item_number -------------------------------------------------------------
+## item_number ------------------------------------------------------------
 # Change the inconsistency of - _ marks.
 nanoblocks$item_number <- nanoblocks$item_number %>% 
   str_replace_all("(.*)_(.*)", "\\1-\\2")
 
-## release_date_japan ------------------------------------------------------
+## release_date_japan -----------------------------------------------------
 # Remove non numeric characters from the date and set it to a date column.
 nanoblocks$release_date_japan <- nanoblocks$release_date_japan %>%
   str_replace("予", "") %>% 
   parse_date("%Y/%m/%d")
 
-## package_size_mm  --------------------------------------------------------
+## package_size_mm  -------------------------------------------------------
 # Remove the (mm) character and spaces between numbers
 nanoblocks$package_size_mm <- nanoblocks$package_size_mm %>% 
   str_replace_all(" ", "") %>% 
-  str_replace("\\(mm\\)", "")
-
-## built_up_size_mm --------------------------------------------------------
+  str_replace("\\(mm\\)", "") %>% 
+  str_trim(side = "right")
+  
+## built_up_size_mm -------------------------------------------------------
 # Although is the same format as package_size_mm, it has more inconsistencies
 # that make harder the cleaning.
 # Need to double check this columns as there might unclean data, or trimmed numbers.
@@ -48,13 +49,13 @@ nanoblocks$built_up_size_mm <- nanoblocks$built_up_size_mm %>%
   str_replace("× ", "×") %>% 
   str_replace(" ", "×")
 
-## pieces_single -----------------------------------------------------------
+## pieces_single ----------------------------------------------------------
 # Remove any character that is not a number.
 nanoblocks$pieces_single <- nanoblocks$pieces_single %>% 
   str_extract("\\d{0,10}") %>% 
   parse_integer()
 
-## pieces_box --------------------------------------------------------------
+## pieces_box -------------------------------------------------------------
 # The format for box sets is [the number of boxes] BOX [number of items in the box] [counter character]
 # Only need is the [number of items in the box]
 # Each box contains 6 items so I can hard-code but cleaning it using Tidyverse
@@ -64,12 +65,12 @@ nanoblocks$pieces_box <- nanoblocks$pieces_box %>%
   str_remove("個") %>% 
   parse_integer()
 
-## Others ------------------------------------------------------------------
+## Others -----------------------------------------------------------------
 # Other columns are already clean
 nanoblocks$target_age
 nanoblocks$difficulty_level
 
-# Clean file ---------------------------------------------------------------
+# Clean file --------------------------------------------------------------
 nanoblocks %>% 
   view()
 
